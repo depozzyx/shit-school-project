28558e80a7f1801f8fe62607ce7e8e4b5703780a
depozzyx <mosentsowivan@gmail.com>
2021-09-07 13:59:28 +0300

    Views start

--- project/shop/views.py
+++ project/shop/views.py
@@ -1,7 +1,13 @@
 # from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse, HttpRequest
+from django.template import loader
 
 
 def index(req: HttpRequest):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    # latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template("shop/index.html")
+    context = {
+        "categories": ["djdidjd", "kkkk"],
+    }
+    return HttpResponse(template.render(context, req))
