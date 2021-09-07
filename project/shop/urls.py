e46c271d0c160af67e08f0004f2885b435e62203
depozzyx <mosentsowivan@gmail.com>
2021-09-07 15:05:06 +0300

    Views more

--- project/shop/urls.py
+++ project/shop/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("category/<int:category_id>/", views.category, name="category"),
+    path("order/<int:product_id>/", views.order, name="order"),
 ]
--- project/shop/views.py
+++ project/shop/views.py
@@ -1,13 +1,40 @@
-# from django.shortcuts import render
-# Create your views here.
+# pylint: disable=no-member
+
 from django.http import HttpResponse, HttpRequest
 from django.template import loader
+
+from . import models
 
 
 def index(req: HttpRequest):
     # latest_question_list = Question.objects.order_by('-pub_date')[:5]
     template = loader.get_template("shop/index.html")
+
+    categories = list(models.Category.objects.all().values())
     context = {
-        "categories": ["djdidjd", "kkkk"],
+        "categories": categories,
+    }
+    return HttpResponse(template.render(context, req))
+
+
+def category(req: HttpRequest, category_id: int):
+    template = loader.get_template("shop/category.html")
+
+    products = list(models.Product.objects.filter(category=category_id).values())
+    category_obj = models.Category.objects.get(id=category_id)
+    print(category_obj)
+    context = {
+        "category": category_obj,
+        "products": products,
+    }
+    return HttpResponse(template.render(context, req))
+
+
+def order(req: HttpRequest, product_id: int):
+    template = loader.get_template("shop/order.html")
+
+    product_obj = models.Product.objects.get(id=product_id)
+    context = {
+        "product": product_obj,
     }
     return HttpResponse(template.render(context, req))
